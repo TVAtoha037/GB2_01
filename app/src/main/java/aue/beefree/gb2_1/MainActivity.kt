2530e093035c238dcab7b18f2ef31b2b8ba2f918
vanya <vanyashinton2013@yandex.ru>
2022-10-10 20:09:46 +0300

    5.a| Сформировал data class WeatherItem с тремя свойствами и вывел их на экран приложения.

--- app/src/main/java/aue/beefree/gb2_1/MainActivity.kt
+++ app/src/main/java/aue/beefree/gb2_1/MainActivity.kt
@@ -3,15 +3,33 @@ package aue.beefree.gb2_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import aue.beefree.gb2_1.R
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 1
+    lateinit var weatherItem: WeatherItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        weatherItem = WeatherItem(System.currentTimeMillis(), 17, "Moscow")
+
         findViewById<Button>(R.id.btn).setOnClickListener{
 
+
+            when(count){
+                1 -> Toast.makeText(applicationContext, weatherItem.time.toString(), Toast.LENGTH_SHORT).show()
+                2 -> Toast.makeText(applicationContext, weatherItem.value.toString(), Toast.LENGTH_SHORT).show()
+                3 -> Toast.makeText(applicationContext, weatherItem.point, Toast.LENGTH_SHORT).show()
+                else -> count = 0
+            }
+
+            count++
         }
+
+
     }
 }
---
+++ app/src/main/java/aue/beefree/gb2_1/WeatherItem.kt
@@ -0,0 +1,4 @@
+package aue.beefree.gb2_1
+
+data class WeatherItem(val time:Long, val value:Int, val point:String){
+}
