cdf29e6a1ef985fd6964eb3613700dc291369961
vanya <vanyashinton2013@yandex.ru>
2022-10-10 19:34:57 +0300

    4| Добавил кнопку в разметку и повесил на неё clickListener в Activity.

--- app/src/main/java/aue/beefree/gb2_1/MainActivity.kt
+++ app/src/main/java/aue/beefree/gb2_1/MainActivity.kt
@@ -2,11 +2,16 @@ package aue.beefree.gb2_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import aue.beefree.gb2_1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn).setOnClickListener{
+
+        }
     }
 }
